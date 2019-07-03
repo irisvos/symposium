35be44d8149608b4c272994cbaf10a4286de6f59
imaginginpsychiatry <52329721+imaginginpsychiatry@users.noreply.github.com>
2019-07-03 15:02:15 +0200

    Update registrations.php

--- register/registrations.php
+++ register/registrations.php
@@ -1,5 +1,8 @@
 <?php
-if($_POST["message"]) {
-    mail("imaginginpsychiatry@gmail.com", "Form to email message", $_POST["message"], "From: imaginginpsychiatry@gmail.com");
+if($_POST["submit"]) {
+    $imaginginpsychiatry@gmail.com($Text12390, $Text12392, $Dropdown12389, "From: $Text12392 <$Text12395>");
+    
+    
+#    mail("imaginginpsychiatry@gmail.com", "Form to email message", $_POST["message"], "From: imaginginpsychiatry@gmail.com");
 }
 ?>
