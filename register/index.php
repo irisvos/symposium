8abd5696e02dc49e22f58183088c6e3317552cad
imaginginpsychiatry <52329721+imaginginpsychiatry@users.noreply.github.com>
2019-07-03 16:03:35 +0200

    Update index.php

--- register/index.php
+++ register/index.php
@@ -26,7 +26,7 @@ $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-type: text/html; charset-utf-8";
 
 $send = mail($to, $subject, $body, $headers);
-if($send){
+if ($send) {
 	echo '<br>';
 	echo 'Registration completed!'
 } else {
