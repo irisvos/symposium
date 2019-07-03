0639f182c7eae4945856eb6932d87878b6a9e494
imaginginpsychiatry <52329721+imaginginpsychiatry@users.noreply.github.com>
2019-07-03 14:49:14 +0200

    Update registrations.php

--- registrations.php
+++ registrations.php
@@ -1,5 +1,5 @@
 <?php
 if($_POST["message"]) {
-    mail("imaginginpsychiatry@gmail.com", "Form to email message", $_POST["message"], "From: an@email.address");
+    mail("imaginginpsychiatry@gmail.com", "Form to email message", $_POST["message"], "From: imaginginpsychiatry@gmail.com");
 }
 ?>
