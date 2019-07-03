6b5fa7c40ae5569cc7f71d574f3a42e4bd2a4e2f
imaginginpsychiatry <52329721+imaginginpsychiatry@users.noreply.github.com>
2019-07-03 16:00:15 +0200

    Update index.php

--- register/index.php
+++ register/index.php
@@ -42,7 +42,8 @@ if($send){
 <!doctype html>
 <html lang="en"><head>
     <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-  </head>
+	<meta charset="utf-8">
+	</head>
   <body>
 	  
  
