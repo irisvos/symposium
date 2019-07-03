51ab9cfabbbeab9a9ba416d9cbe278419e9a0ff6
imaginginpsychiatry <52329721+imaginginpsychiatry@users.noreply.github.com>
2019-07-03 15:09:45 +0200

    Update registrations.php

--- register/registrations.php
+++ register/registrations.php
@@ -1,11 +1,10 @@
-
 <?php
-if($_POST["submit"]) {
-    $recipient = "imaginginpsychiatry@gmail.com"; //my email
-    $Text12392 = $_POST ["yourName"];
-    $Text12395 = $_POST["yourEmail"];
+// The message
+$message = "New registration";
 
-    $mail($recipient, "From: $Text12392 <$Text12395>");
-    $thankYou="<p>Thank you! We will be in contact with you shortly.</p>";
-}
+// In case any of our lines are larger than 70 characters, we should use wordwrap()
+$message = wordwrap($message, 70, "\r\n");
+
+// Send
+mail('imaginginpsychiatry@gmail.com', 'Iris Vos', $message);
 ?>
